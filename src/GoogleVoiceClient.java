0b3f9acaaf35626ea61c773feba158a6db7a8196
Hayden Waisanen <haydenzone@gmail.com>
2012-12-16 23:21:42 -0700

    Copying over previously developed code

---
+++ src/GoogleVoiceClient.java
@@ -0,0 +1,203 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import java.util.*;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import com.techventus.server.voice.Voice;
+import com.techventus.server.voice.datatypes.records.*;
+
+public class GoogleVoiceClient {
+	public static Voice voice;
+	private static Map<String, Notification> currentNotifications = Collections
+			.synchronizedMap(new HashMap<String, Notification>());
+
+	public static void main(String[] args) throws InterruptedException, IOException {
+		String userName = "";
+		String pass = "";
+
+		// Input the userName and password
+		userName = getUsername();
+		pass = getPassword();
+		
+		//Attempt to login 
+		try {
+			voice = new Voice(userName, pass);
+		}
+		catch (IOException e) {
+			// Print out the exception that occurred
+			JOptionPane.showMessageDialog(null,
+				    "Incorrect username or password. Exiting...",
+				    "Google Voice Desktop Client",
+				    JOptionPane.ERROR_MESSAGE);
+			System.out.println(e.getMessage());
+			System.exit(0);
+		}
+		
+		//Loop through threads and create popups for unread messages
+		for(SMSThread thread : voice.getSMSThreads()) {
+			if (!thread.getRead()) {
+				Notification popUp = new Notification(thread);
+				popUp.launch();
+				currentNotifications.put(thread.getId(), popUp);
+			}
+		}
+
+		// Add an icon to the system tray if supported
+		if (!SystemTray.isSupported()) {
+			System.out.println("SystemTray is not supported");
+			// Create a simple window to kill the application if need be.
+			TinyWindow win = new TinyWindow();
+			win.setVisible(true);
+			win.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		} 
+		else {
+			createSystemTrayIcon();
+		}
+
+		// Enter query loop on server
+		while (true) {
+			
+			//Pull down current list of threads from server
+			Collection<SMSThread> new_threads = voice.getSMSThreads();
+			
+			//Loop through all threads
+			for (SMSThread thread : new_threads) {
+				// Check to see if the thread is read or not
+				if (thread.getRead()) { // Is read
+					// Check to see if a popup exists
+					if (currentNotifications.containsKey(thread.getId())) {
+						// Kill popups for this thread
+						killPopUp(thread.getId());
+					}
+
+				} else { // Is unread
+							// Check to see if a popup exists
+					if (currentNotifications.containsKey(thread.getId())) {
+						// Update the threads pop
+						currentNotifications.get(thread.getId())
+								.update_thread(thread);
+					} else {// No pop up
+							// Create the popup
+						Notification new_notif = new Notification(thread);
+						new_notif.launch();
+						currentNotifications.put(thread.getId(), new_notif);
+					}
+
+				}
+			}
+			Thread.sleep(1000);
+		}
+
+	}
+
+	public static void createSystemTrayIcon() {
+		final PopupMenu popup = new PopupMenu();
+		
+		
+		//Load the icon
+		Image image = null;
+		try {
+			ClassLoader classLoader = Thread.currentThread()
+					.getContextClassLoader();
+			InputStream input = classLoader
+					.getResourceAsStream("gv_small.gif");
+			image = ImageIO.read(input);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		//Set the icon
+		final TrayIcon trayIcon = new TrayIcon(image,
+				"Google Voice Desktop Client");
+		
+		//Get the tray
+		final SystemTray tray = SystemTray.getSystemTray();
+		// Create a menu for the icon
+		MenuItem exitItem = new MenuItem("Exit");
+		exitItem.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				System.exit(0);
+
+			}
+		});
+
+		// Add button to menu
+		popup.add(exitItem);
+
+		//Register popup menu with trayIcon
+		trayIcon.setPopupMenu(popup);
+
+		//Add it to the tray
+		try {
+			tray.add(trayIcon);
+		} catch (AWTException e) {
+			System.out.println("TrayIcon could not be added.");
+		}
+		
+	}
+	public static void killPopUp(String threadID) {
+		currentNotifications.containsKey(threadID);
+		Notification toKill = currentNotifications.get(threadID);
+		toKill.dispose();
+
+		// Hold the threadID on the currentNotifications list for
+		//  a second to prevent an incoming SMSthreads update
+		// from recreating it.
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		currentNotifications.remove(threadID);
+	}
+
+	public static void markMessageAsRead(String id) {
+		try {
+			voice.markAsRead(id);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static boolean sendText(String toNum, String sentMsg)
+			throws IOException {
+		voice.sendSMS(toNum, sentMsg);
+		return true;
+	}
+
+	public static String getUsername() {
+		return JOptionPane.showInputDialog(null, "UserName", "Google Voice Desktop Client",
+				JOptionPane.QUESTION_MESSAGE);
+	}
+	public static String getPassword() {
+		JPanel panel = new JPanel();
+		JLabel label = new JLabel("Enter a password:");
+		JPasswordField pass_field = new JPasswordField(10);
+		panel.add(label);
+		panel.add(pass_field);
+		String[] options = new String[] { "OK", "Cancel" };
+		JOptionPane.showOptionDialog(null, panel, "Google Voice Desktop Client",
+				JOptionPane.NO_OPTION, JOptionPane.PLAIN_MESSAGE, null,
+				options, options[1]);
+		return String.copyValueOf(pass_field.getPassword());
+	}
+}
+
+@SuppressWarnings("serial")
+class TinyWindow extends JFrame {
+	public TinyWindow() {
+		Container c = getContentPane();
+		c.setLayout(new FlowLayout());
+		JButton close = new JButton("Close");
+		close.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+		c.add(close);
+		pack();
+	}
+}
---
+++ src/Notification.java
@@ -0,0 +1,148 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Random;
+import javax.swing.*;
+import com.techventus.server.voice.datatypes.records.SMS;
+import com.techventus.server.voice.datatypes.records.SMSThread;
+
+@SuppressWarnings("serial")
+public class Notification extends JFrame implements Runnable {
+	private SMSThread thread;
+	private JTextArea message_box;
+	private JTextArea message_input;
+
+	public void run() {
+		setVisible(true);
+	}
+
+	public static String getMostRecentMessage(SMSThread thread) {
+		Collection<SMS> messages = thread.getAllSMS();
+		String message = messages.iterator().next().getContent();
+		return message;
+	}
+
+	public Notification(SMSThread toShow) {
+		thread = toShow;
+		createPopUp(toShow.getContact().getName(),
+				Notification.getMostRecentMessage(toShow));
+	}
+
+	public void update_thread(SMSThread newThread) {
+		thread = newThread;
+		message_box.setText(getMostRecentMessage(newThread));
+
+	}
+
+	public void createPopUp(String from, String message) {
+		setMaximumSize(new Dimension(300, 100));
+		setAlwaysOnTop(true);
+		Container c = getContentPane();
+		c.setLayout(new BorderLayout());
+		Container north = new Container();
+		north.setLayout(new FlowLayout());
+
+		// Display who the message is from
+		north.add(new JLabel("From: " + from));
+		c.setBackground(Color.GREEN);
+		c.add(north, BorderLayout.NORTH);
+		message_box = new JTextArea(6, 20);
+		message_box.setLineWrap(true);
+		message_box.setEditable(false);
+		message_box.setText(message);
+		c.add(message_box, BorderLayout.CENTER);
+
+		// Create the reply section
+		JPanel reply_area = new JPanel();
+		reply_area.setLayout(new FlowLayout());
+		message_input = new JTextArea(6, 20);
+		message_input.setLineWrap(true);
+		JScrollPane scrollable_input = new JScrollPane(message_input);
+		JButton reply_button = new JButton("Reply");
+		ActionListener send = new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				send();
+			}
+		};
+		message_input.addKeyListener(new KeyListener() {
+			public void keyPressed(KeyEvent e) {
+				if (e.getKeyCode() == KeyEvent.VK_ENTER)
+					send();
+			}
+
+			@Override
+			public void keyReleased(KeyEvent e) {
+			}
+
+			@Override
+			public void keyTyped(KeyEvent e) {
+			}
+		});
+		reply_button.addActionListener(send);
+		reply_area.add(scrollable_input);
+		reply_area.add(reply_button);
+		c.add(reply_area, BorderLayout.SOUTH);
+
+		// Register a close listener
+		WindowListener exitListener = new WindowAdapter() {
+
+			@Override
+			public void windowClosing(WindowEvent e) {
+				GoogleVoiceClient.markMessageAsRead(thread.getId());
+				GoogleVoiceClient.killPopUp(thread.getId());
+				dispose();
+			}
+		};
+		addWindowListener(exitListener);
+		pack();
+		// Select the window location
+		Random gen = new Random(System.currentTimeMillis());
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		Dimension windowSize = getSize();
+		int x_lower_bound = (int) (screenSize.getWidth()
+				- windowSize.getWidth() - 200);
+		int windowX = Math.max(
+				0,
+				x_lower_bound
+						+ gen.nextInt(screenSize.width - windowSize.width
+								- x_lower_bound));
+		int windowY = Math.max(0,
+				gen.nextInt(screenSize.height - windowSize.height));
+		setLocation(windowX, windowY);
+	}
+
+	public void launch() {
+		Thread t = new Thread(this, "My Thread");
+		t.start();
+
+	}
+
+	public void send() {
+		String toSend = message_input.getText();
+		if (valid_message(toSend)) {
+			System.out.println("Sending message \"" + toSend + "\" to "
+					+ thread.getContact().getNumber());
+
+			GoogleVoiceClient.killPopUp(thread.getId());
+			try {
+				GoogleVoiceClient.sendText(thread.getContact().getNumber(), toSend);
+			} catch (IOException ex) {
+				System.out.println(ex.getMessage());
+			}
+		}
+
+	}
+
+	private boolean valid_message(String message) {
+		if (message.equals("")) {
+			return false;
+		}
+		return true;
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
